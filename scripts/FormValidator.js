e0b5461bb85bfb21e46e615897033756cc59f0a0
Anton Romanetc <antony.romanetc@yandex.ru>
2022-06-04 18:18:31 +0500

    hotfix

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,12 +1,3 @@
-export const formsValidationConfig = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__text",
-  submitButtonSelector: ".popup__submit-button",
-  inactiveButtonClass: "popup__submit-button_disabled",
-  inputErrorClass: "popup__text-type_error",
-  errorClass: "popup__text-error_active",
-}
-
 export default class FormValidator {
   constructor (settings, form) {
     this._form = form;
